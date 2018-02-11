699cb51a77296081ffd572a769356a4d14c1cdaf
Aralya Phinith <aphinith84@gmail.com>
2018-02-11 11:54:07 -0800

    add a png, gif, jpg, and svg image to test if images will render onto page

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import './src/style.scss'
+import drStrangeJpg from './src/assets/drstrange.jpg'
+import hamtaroPng from './src/assets/hamtaro.png'
+import hulkGif from './src/assets/hulk.gif'
+import anchorSvg from './src/assets/anchor.svg'
 
 class Main extends Component {
 
@@ -8,6 +12,10 @@ class Main extends Component {
     return(
       <div>
         Show me a photo gallery!
+        <img src={drStrangeJpg} className="pic"/>
+        <img src={hamtaroPng} className="pic" />
+        <img src={hulkGif} className="pic" />
+        <img src={anchorSvg} className="pic" />
       </div>
     );
   }
