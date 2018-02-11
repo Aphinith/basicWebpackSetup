bafcb463e0a3fe3ebf3be38a1973df900f0e00bb
Aralya Phinith <aphinith84@gmail.com>
2018-02-11 10:54:32 -0800

    update webpack config file to use babel loader to load both .js and .jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: 'index.js',
+  entry: './index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        exclude: '/node_modules'
+      }
+    ]
+  },
+  stats: {
+    colors: true
   }
 };
