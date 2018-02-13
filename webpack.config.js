6e6209003bec1551dfd1ca23f1eceb03ae241f86
Aralya Phinith <aphinith84@gmail.com>
2018-02-12 23:03:50 -0800

    create a hash css file for output

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const FileSystem = require('fs');
 
 module.exports = {
   entry: './index.js',
@@ -20,16 +22,10 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [
-        {
-          loader: 'style-loader'
-        }, 
-        {
-          loader: 'css-loader'
-        },
-        {
-          loader: 'sass-loader'
-        }]
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader!sass-loader'
+        })
       },
       {
         test: /\.(png|jpg|gif)$/,
@@ -53,5 +49,26 @@ module.exports = {
   },
   stats: {
     colors: true
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('css/[hash]bundle.css'),
+    // below code is meant to overwrite the script tag to use the new bundled file, but is not working properly
+    function() {
+      this.plugin("done", function(statsData) {
+        var stats = statsData.toJson();
+        if (!stats.errors.length) {
+          var htmlFileName = "index.html";
+          var html = FileSystem.readFileSync(path.join(__dirname, 'dist', htmlFileName), "utf8");
+
+          var htmlOutput = html.replace(
+              /<script\s+src=(["'])(.+?)bundle\.js\1/i,
+              "<script src=$1$2" + stats.assetsByChunkName.main[0] + "$1");
+
+          FileSystem.writeFileSync(
+              path.join(__dirname, "dist", htmlFileName),
+              htmlOutput);
+        }
+      });
+    }
+  ]
 };
