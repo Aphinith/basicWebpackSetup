2766e8303c5d92a5de2210c2a18c6331a0366c3f
Aralya Phinith <aphinith84@gmail.com>
2018-02-13 00:18:32 -0800

    update webpack config file to handle jpeg files, include jpeg image into index.js to test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import './src/style.scss'
+import deadPoolJpeg from './src/assets/deadpool.jpeg'
 import drStrangeJpg from './src/assets/drstrange.jpg'
 import hamtaroPng from './src/assets/hamtaro.png'
 import hulkGif from './src/assets/hulk.gif'
@@ -7,6 +8,9 @@ import anchorSvg from './src/assets/anchor.svg'
 function component() {
   const element1 = document.createElement('div');
   element1.innerHTML = "Show me a photo gallery!";
+
+  const img0 = document.createElement('img');
+  img0.src = deadPoolJpeg;
 
   const img1 = document.createElement('img');
   img1.src = drStrangeJpg;
@@ -20,6 +24,7 @@ function component() {
   const img4 = document.createElement('img');
   img4.src = anchorSvg;
 
+  element1.appendChild(img0);
   element1.appendChild(img1);
   element1.appendChild(img2);
   element1.appendChild(img3);
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         })
       },
       {
-        test: /\.(png|jpg|gif)$/,
+        test: /\.(png|jpg|jpeg|gif)$/,
         use: [
         {
           loader: 'file-loader'
