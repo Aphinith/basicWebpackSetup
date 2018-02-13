6b6bdb175989ed98d3ed845952a2ec21892486a2
Aralya Phinith <aphinith84@gmail.com>
2018-02-13 00:03:59 -0800

    update index.js file to render text and images onto page, update scss file to apply correct styling to elements

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// import React, { Component } from 'react'
-// import { render } from 'react-dom'
 import './src/style.scss'
 import drStrangeJpg from './src/assets/drstrange.jpg'
 import hamtaroPng from './src/assets/hamtaro.png'
@@ -31,34 +29,3 @@ function component() {
 }
 
 document.body.appendChild(component());
-
-// class Main extends Component {
-
-//   render() {
-//     return(
-//       <div>
-//         <div>
-//           Show me a photo gallery!
-//         </div>
-//         <div>
-//           <img src={drStrangeJpg} className="pic"/>
-//           <p>This is a jpg file</p>
-//         </div>
-//         <div>
-//           <img src={hamtaroPng} className="pic" />
-//           <p>This is a png file</p>
-//         </div>
-//         <div>
-//           <img src={hulkGif} className="pic" />
-//           <p>This is a gif file</p>
-//         </div>
-//         <div>
-//           <img src={anchorSvg} className="pic" />
-//           <p>This is an svg file</p>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// render(<Main />, document.body.appendChild(Main))
